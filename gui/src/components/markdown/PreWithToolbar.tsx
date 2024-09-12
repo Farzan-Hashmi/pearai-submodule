ec63f0ddefb3a8ad719ada1a078c5dd0db64cd4c
Farzan Hashmi <farzan.hashmi@berkeley.edu>
2024-09-12 05:35:16 -0700

    refactor

--- gui/src/components/markdown/PreWithToolbar.tsx
+++ gui/src/components/markdown/PreWithToolbar.tsx
@@ -19,6 +19,53 @@ function childrenToText(children: any) {
   return children.map((child: any) => childToText(child)).join("");
 }
 
+interface CollapseButtonProps {
+  collapsed: boolean;
+  onClick: () => void;
+  position: "top" | "bottom";
+}
+
+const CollapseButton: React.FC<CollapseButtonProps> = ({
+  collapsed,
+  onClick,
+  position,
+}) => {
+  const topStyle = position === "top" ? { top: "-9px" } : { bottom: "-10px" };
+
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        position: "absolute",
+        left: collapsed ? "-11px" : "-2px",
+        backgroundColor: "transparent",
+        color: "lightgray",
+        cursor: "pointer",
+        fontSize: "12px",
+        fontWeight: "600",
+        border: "none",
+        ...topStyle,
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="currentColor"
+      >
+        {collapsed ? (
+          <path d="M6.999,0v16h2.002V0H6.999z M12.172,9H10V7h2.172l-1.586-1.586L12,4l4,4l-4,4l-1.414-1.414L12.172,9z" />
+        ) : position === "top" ? (
+          <path d="M0,6.999v2.002h16V6.999H0z M9,12.172V10H7v2.172l-1.586-1.586L4,12l4,4l4-4l-1.414-1.414L9,12.172z" />
+        ) : (
+          <path d="M0,6.999v2.002h16V6.999H0z M7,3.828V6h2V3.828l1.586,1.586L12,4L8,0L4,4l1.414,1.414L7,3.828z" />
+        )}
+      </svg>
+    </button>
+  );
+};
+
 function PreWithToolbar(props: {
   children: any;
   language: string | undefined;
@@ -53,78 +100,38 @@ function PreWithToolbar(props: {
       onMouseEnter={() => setHovering(true)}
       onMouseLeave={() => setHovering(false)}
     >
-      {/* Collapse button at the top */}
-      <button
+      <CollapseButton
+        collapsed={collapsed}
         onClick={toggleCollapse}
-        style={{
-          position: "absolute",
-          top: "-9px",
-          left: collapsed ? "-11px" : "-2px",
-          backgroundColor: "transparent",
-          color: "lightgray",
-          cursor: "pointer",
-          fontSize: "12px",
-          fontWeight: "600",
-          border: "none",
-        }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-        >
-          {collapsed ? (
-            <path d="M6.999,0v16h2.002V0H6.999z M12.172,9H10V7h2.172l-1.586-1.586L12,4l4,4l-4,4l-1.414-1.414L12.172,9z" />
-          ) : (
-            <path d="M0,6.999v2.002h16V6.999H0z M9,12.172V10H7v2.172l-1.586-1.586L4,12l4,4l4-4l-1.414-1.414L9,12.172z" />
-          )}
-        </svg>
-      </button>
+        position="top"
+      />
 
-      {!collapsed && !toolbarBottom && hovering && (
-        <CodeBlockToolBar
-          text={copyValue}
-          bottom={toolbarBottom}
-          language={props.language}
-        />
-      )}
-      {!collapsed && props.children}
-      {!collapsed && toolbarBottom && hovering && (
-        <CodeBlockToolBar
-          text={copyValue}
-          bottom={toolbarBottom}
-          language={props.language}
-        />
-      )}
-
-      {/* Collapse button at the bottom */}
       {!collapsed && (
-        <button
-          onClick={toggleCollapse}
-          style={{
-            position: "absolute",
-            bottom: "-10px",
-            left: collapsed ? "-11px" : "-2px",
-            backgroundColor: "transparent",
-            color: "lightgray",
-            cursor: "pointer",
-            fontSize: "12px",
-            fontWeight: "600",
-            border: "none",
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-          >
-            <path d="M0,6.999v2.002h16V6.999H0z M7,3.828V6h2V3.828l1.586,1.586L12,4L8,0L4,4l1.414,1.414L7,3.828z" />
-          </svg>
-        </button>
+        <>
+          {!toolbarBottom && hovering && (
+            <CodeBlockToolBar
+              text={copyValue}
+              bottom={toolbarBottom}
+              language={props.language}
+            />
+          )}
+
+          {props.children}
+
+          {toolbarBottom && hovering && (
+            <CodeBlockToolBar
+              text={copyValue}
+              bottom={toolbarBottom}
+              language={props.language}
+            />
+          )}
+
+          <CollapseButton
+            collapsed={collapsed}
+            onClick={toggleCollapse}
+            position="bottom"
+          />
+        </>
       )}
     </div>
   );
